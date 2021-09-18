e33893219a621ec87b0c8aeea353d53d5b0a38d4
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-17 22:24:23 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entitites.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -19,25 +21,39 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	@Transactional (readOnly = true)      //garantia das propriedades ACID
+	@Transactional(readOnly = true) // garantia das propriedades ACID
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 	// optional usado para evitar valores nulos vindos do findById
-	@Transactional (readOnly = true)      //garantia das propriedades ACID
+	@Transactional(readOnly = true) // garantia das propriedades ACID
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
-		entity.setName(dto.getName());       //daqui abaixo coloca os demais campos, caso tenha na entidade
+		entity.setName(dto.getName()); // daqui abaixo coloca os demais campos, caso tenha na entidade
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	
+	//Método para atualizar um dado nas entidades e trata exceção
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id); // instancia com getOne antes de salvar
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found "+ id);
+		}
 	}
 }

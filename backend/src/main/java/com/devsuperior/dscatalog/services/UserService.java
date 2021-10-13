ad767562851bc510f10da1cff8906dc822493d22
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-10-13 10:05:43 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,103 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entitites.Role;
+import com.devsuperior.dscatalog.entitites.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+
+	@Transactional(readOnly = true) // garantia das propriedades ACID
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> list = repository.findAll(pageable);
+		return list.map(x -> new UserDTO(x));
+	}
+
+	// optional usado para evitar valores nulos vindos do findById
+	@Transactional(readOnly = true) // garantia das propriedades ACID
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtotoEntity(dto, entity);          // cria um metodo que relaciona os campos para ocupar no insert e update
+										       // ou coloca os demais campos, caso tenha mais de um na entidade
+		
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	// Método para atualizar um dado nas entidades e trata exceção
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);     // instancia com getOne antes de salvar sem ir no banco
+			copyDtotoEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id); // criada no services.exceptions
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation"); // criada no services.exceptions
+		}
+	}
+
+	// copiador dos dados dos atributos para insert e update //não precisa fazer
+	// para os atributos chave
+	private void copyDtotoEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+
+		entity.getRoles().clear();     // limpa a lista de role anterior, caso tenha
+
+		// busca o id de cada role
+		for (RoleDTO roleDto : dto.getRoles()) { 
+			Role role = roleRepository.getOne(roleDto.getId()); // import entities.role
+			entity.getRoles().add(role);
+		}
+	}
+}
